e6fb419f96feb459305e837dde43bcc70dd2e46f
Ilya Zlobintsev <ilya.zl@protonmail.com>
2021-07-10 17:35:38 +0300

    Revert "Lastfm authentication" because this isn't needed
    This reverts commit 17b1fa53ae9692d2ce01d18ff89017cdfd8ad9ce.

--- src/web.rs
+++ src/web.rs
@@ -53,8 +53,6 @@ pub async fn run(command_handler: CommandHandler) -> JoinHandle<()> {
                     authenticate::authenticate_spotify,
                     authenticate::spotify_redirect,
                     authenticate::disconnect_spotify,
-                    authenticate::authenticate_lastfm,
-                    authenticate::lastfm_redirect,
                     authenticate::logout,
                 ],
             )
