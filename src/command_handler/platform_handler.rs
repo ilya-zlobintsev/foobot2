9185dd50ee61ca18ad98d925c296b28d89a78bff
Ilya Zlobintsev <ilya.zl@protonmail.com>
2022-03-02 21:26:17 +0200

    feat: rework server message chunking

--- src/command_handler/platform_handler.rs
+++ src/command_handler/platform_handler.rs
@@ -39,24 +39,20 @@ impl PlatformHandler {
                 let msg = msg.split_whitespace().collect::<Vec<&str>>().join(" ");
 
                 if msg.len() > twitch::MSG_LENGTH_LIMIT {
-                    let msg_bytes = msg.into_bytes();
-                    match msg_bytes
-                        .chunks(twitch::MSG_LENGTH_LIMIT)
-                        .map(std::str::from_utf8)
-                        .collect::<Result<Vec<&str>, _>>()
-                    {
-                        Ok(chunks) => {
-                            for chunk in chunks {
-                                chat_client
-                                    .privmsg(broadcaster.login.clone(), chunk.to_string())
-                                    .await?;
-                                sleep(Duration::from_millis(500)).await; // scuffed rate limiting
-                            }
-                        }
+                    let mut section = String::new();
 
-                        Err(e) => {
-                            chat_client.privmsg(broadcaster.login, format!("UTF8 Error: {}", e)).await?;
+                    for ch in msg.chars() {
+                        section.push(ch);
+                        if section.len() == twitch::MSG_LENGTH_LIMIT {
+                            chat_client
+                                .privmsg(broadcaster.login.clone(), section.clone())
+                                .await?;
+                            section.clear();
+                            sleep(Duration::from_millis(500)).await; // scuffed rate limiting
                         }
+                    }
+                    if !section.is_empty() {
+                        chat_client.privmsg(broadcaster.login, section).await?;
                     }
                 } else {
                     chat_client.privmsg(broadcaster.login, msg).await?;
