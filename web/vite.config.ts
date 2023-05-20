f199953aa1f33b59b2e1262d004b3913edc229f1
Ilya Zlobintsev <ilya.zl@protonmail.com>
2023-05-20 23:15:23 +0300

    fixes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,22 +3,22 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const baseUrl = mode === "development" ? "http://localhost:8000" : "";
 
     return {
-        plugins: [svelte({
-            extensions: [
-                '.svelte', '.svx', '.md'
-            ],
-            preprocess: [
-                mdsvex({ extensions: ['svx', '.md'] }),
-                preprocess(),
-            ],
-        })],
+        plugins: [
+            svelte({
+                extensions: [
+                    '.svelte', '.svx', '.md'
+                ],
+                preprocess: [
+                    mdsvex({ extensions: ['svx', '.md'] }),
+                    preprocess(),
+                ],
+            })
+        ],
         define: {
             BASE_URL: JSON.stringify(baseUrl)
         }
